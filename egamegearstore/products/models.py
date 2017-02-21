5d08f1bab4c1bc4b8f72937c685d7e73fedd1ee3
Lord Hung <hunguit14@gmail.com>
2017-02-21 15:27:02 +0700

    [ADD] Add product variation

--- egamegearstore/products/models.py
+++ egamegearstore/products/models.py
@@ -29,12 +29,35 @@ class Product(models.Model):
 
     objects = ProductManager()
 
-    def __str__(self):  # giống với def __unicode__(self): trong python2
+    def __str__(self):  # giống với def __unicode__(self),__init__: trong python2
         return self.title
 
     # lấy đường dẫn tuyệt đối product_detail
     def get_absolute_url(self):
         return reverse("product_detail", kwargs={"pk": self.pk})
+
+
+class Variation(models.Model):
+    product = models.ForeignKey(Product)
+    title = models.CharField(max_length=120)
+    price = models.DecimalField(decimal_places=2, max_digits=20)
+    sale_price = models.DecimalField(
+        max_digits=20, decimal_places=2, null=True, blank=True)
+    active = models.BooleanField(default=True)
+    # refer none == unlimited amount
+    inventory = models.IntegerField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+    def get_price(self):
+        if self.price is not None:
+            return self.sale_price
+        else:
+            return self.price
+
+    def get_absolute_url(self):
+        return self.product.get_absolute_url()
 
     # Product Images
 
