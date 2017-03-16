64e5f5835c968af16cf75cbf483a82bbe3e8a3c4
Lord Hung <hunguit14@gmail.com>
2017-03-16 18:16:37 +0700

    [ADD] Add Product Editing with DjangoAdminInline

--- egamegearstore/products/admin.py
+++ egamegearstore/products/admin.py
@@ -1,9 +1,31 @@
 from django.contrib import admin
 from .models import Product, Variation, ProductImage, Category, ProductFeatured
 
+
+class VariationInline(admin.TabularInline):
+    model = Variation
+    # Show only one variation to add, can append more
+    extra = 0
+    max_num=10
+
+
+class ProductImage(admin.TabularInline):
+    model = ProductImage
+    extra =0
+    max_num=10
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'price']
+    inlines = [VariationInline,ProductImage]
+
+    class Meta:
+        model = Product
+
+
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Variation)
-admin.site.register(ProductImage)
+admin.site.register(Product, ProductAdmin)
+# admin.site.register(Variation)
+# admin.site.register(ProductImage)
 admin.site.register(Category)
 admin.site.register(ProductFeatured)
