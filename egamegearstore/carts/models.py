8e654b07a20c89b625602c68ac277bd07a7d96f8
Lord Hung <hunguit14@gmail.com>
2017-03-16 19:03:13 +0700

    [ADD] Add Carts app

---
+++ egamegearstore/carts/models.py
@@ -0,0 +1,30 @@
+from django.conf import settings
+from django.db import models
+
+from products.models import Variation
+# Create your models here.
+
+
+class CartItem(models.Model):
+    item = models.ForeignKey(Variation)
+    quantity = models.PositiveIntegerField(default=1)
+    # line item total
+
+    def __str__(self):
+        return self.item.title
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, null=True, blank=True)
+    itemList = models.ManyToManyField(CartItem)
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    # subtotal price
+    # taxes total
+    # discounts
+    # total price
+
+    def __str__(self):
+        """TODO: to be defined1. """
+        return str(self.id)
