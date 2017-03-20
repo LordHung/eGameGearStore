9b1be493c6e69ce7fe0225518161447cccfa9f63
Lord Hung <hunguit14@gmail.com>
2017-03-20 21:24:25 +0700

    [ADD] Use Django session to retrive cart_id

--- egamegearstore/carts/views.py
+++ egamegearstore/carts/views.py
@@ -9,12 +9,23 @@ from carts.models import Cart, CartItem
 class CartView(View):
 
     def get(self, request, *args, **kwargs):
-        item_id = request.GET.get("item")
+        request.session.set_expiry(3)  # second
+        cart_id = request.session.get('cart_id')
+        if cart_id is None:
+            cart = Cart()
+            cart.save()
+            cart_id = cart.id
+            request.session['cart_id'] = cart_id
+        cart = Cart.objects.get(id=cart_id)
+        # bind request user to cart.user
+        if request.user.is_authenticated():
+            cart.user = request.user
+            cart.save()
+        item_id = request.GET.get('item')
         delete = request.GET.get('delete')
         if item_id:
             item_instance = get_object_or_404(Variation, id=item_id)
             qty = request.GET.get("qty")
-            cart = Cart.objects.all().first()
             cart_item = CartItem.objects.get_or_create(
                 cart=cart, item=item_instance)[0]
             if delete:
