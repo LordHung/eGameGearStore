504adaaf501c5cc363a8a286f0acdada3084b710
Lord Hung <hunguit14@gmail.com>
2017-04-15 23:52:46 +0700

    [ADD] Add UserAddress Model

--- egamegearstore/orders/models.py
+++ egamegearstore/orders/models.py
@@ -14,6 +14,24 @@ class UserCheckout(models.Model):
         return self.email
 
 
+# billing: store in db; Billing: display on form
+ADDRESS_TYPE = (
+    ('billing', 'Billing'),
+    ('shipping', 'Shipping')
+)
+
+
+class UserAddress(models.Model):
+    user = models.ForeignKey(UserCheckout)
+    type = models.CharField(max_length=50, choices=ADDRESS_TYPE)
+    street = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    zipcode = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.street
+
 # class Order(models.Model):
     # order_id
     # cart
